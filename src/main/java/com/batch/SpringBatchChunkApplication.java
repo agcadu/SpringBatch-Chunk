734e7aa5c88c5b588399b32c344447945a460c30
Agustin <agcadu@gmail.com>
2023-07-25 13:30:34 +0200

    inicio del proyecto

---
+++ src/main/java/com/batch/SpringBatchChunkApplication.java
@@ -0,0 +1,13 @@
+package com.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchChunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchChunkApplication.class, args);
+	}
+
+}
